417b3447c39aa951e8b392b821703a12921c75f7
Tony Li <tonyli.lives@gmail.com>
2018-01-29 18:32:36 -0500

    comment the script

--- bin/gen-references.sh
+++ bin/gen-references.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
+# test the regex here:
+# https://regex101.com/r/ETiz0z/1
 REGEX='s/   (--(\S* \S+)|(\S*))\s+(\b(.*?))? \B(\(default\: (.*?)\))? ?\B\[(\$.*?)\]/`$1$3` <br><br> `$8` | $5 <br><br> `$7 `/g'
 
+# apply the regex and cleanup the outputs
 docker run -it drone/drone:latest -h | perl -i -pe "$REGEX" | grep '<br>' > server.txt
-
 docker run -it drone/agent:latest -h | perl -i -pe "$REGEX" | grep '<br>' > agent.txt

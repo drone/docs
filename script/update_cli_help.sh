7d7878ae8aa3446a8ed282e6fefd84a41e6c4cd5
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2018-06-07 09:45:08 +0900

    Improve update_cli_help.sh
    * Output the drone cli version
    * Add a comment
    * Exit when a command is failure

--- script/update_cli_help.sh
+++ script/update_cli_help.sh
@@ -1,7 +1,15 @@
-cd `dirname $0`/..
+# Update drone cli's each command's helps
+# When the new version of drone cli has released,
+# update the local drone cli and run this script.
+#
+# USAGE:
+#   $ bash script/update_cli_help.sh
+
+drone --version || exit 1
+cd `dirname $0`/.. || exit 1
 
 for file in `find content/cli -name "*.out.txt"`; do
   COMMAND=`basename $file | sed -e "s/drone_\(.*\)\.out\.txt/\1/" | sed -e "s/_/ /g"`
   echo "+ drone $COMMAND --help > $file"
-  drone $COMMAND --help > $file
+  drone $COMMAND --help > $file || exit 1
 done
